b0b9d6ea081439addbd4315e8f42eaa1e6aa3abb
musakaras <32721242+musakaras@users.noreply.github.com>
2022-03-02 02:24:01 +0300

    Add files via upload

---
+++ EtkinlikOner.java
@@ -0,0 +1,30 @@
+package PatikaJava;
+
+import java.util.Scanner;
+
+public class EtkinlikOner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hava Sıcaklığını Giriniz: ");
+        int temperature = scan.nextInt();
+
+        if (temperature < 5)
+        {
+            System.out.println("Kayak Yapabilirsiiniz");
+        }
+        else if (temperature > 5){
+            if (temperature<15) {
+                System.out.println("Sinemaya Gidebilirsiniz");
+            }
+            else if (temperature>15 && temperature <25)
+            {
+                System.out.println("Pikniğe Gidebilirsiniz");
+            }
+            else if (temperature>25){
+                System.out.println("Yüzmeye Gidebilirsiniz");
+            }
+        }
+
+    }
+}
