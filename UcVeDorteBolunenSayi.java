5bcf232822af6e16a5ba5295804ae09a657b4e37
musakaras <32721242+musakaras@users.noreply.github.com>
2022-03-02 17:10:05 +0300

    Add files via upload

---
+++ UcVeDorteBolunenSayi.java
@@ -0,0 +1,28 @@
+package PatikaJava;
+
+
+import java.util.Scanner;
+
+public class UcVeDorteBolunenSayi  {
+    public static void main(String[] args) {
+
+        int sum =0; int valueNumber=0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int number = scanner.nextInt();
+
+        for (int i=0; i<number;i++) {
+            if (i%3==0 && i%4==0) {
+                System.out.println(i);
+                sum +=i;
+                valueNumber++;
+            }
+
+        }
+
+        System.out.println("Toplam: "+sum);
+        double avarege= sum / valueNumber;
+        System.out.print("Avarage: "+avarege);
+
+    }
+}
